faa356a5961f1d54a12259ab86da90d025ef444b
Marcin Nowicki <pr0d1r2@gmail.com>
2020-09-19 09:25:57 +0200

    Update cask file syntax

--- Casks/xonotic.rb
+++ Casks/xonotic.rb
@@ -1,11 +1,10 @@
-cask :v1 => 'xonotic' do
+cask 'xonotic' do
   version '0.8.0'
   sha256 '0a87c808827a8b234f2324b468ecb643aa48a6fd43322a29e616610dca316370'
 
   url "http://dl.xonotic.org/xonotic-#{version}.zip"
   name 'Xonotic'
   homepage 'http://www.xonotic.org/'
-  license :gpl
 
   postflight do
     frameworks_dir = "#{ENV['HOME']}/Library/Frameworks"
