3a62006ec4357e45f5a0770e562d481d7633e323
Marcin Nowicki <pr0d1r2@gmail.com>
2020-09-19 09:28:39 +0200

    Add missing version to formula

--- brew-cask-games.rb
+++ brew-cask-games.rb
@@ -3,6 +3,7 @@ require 'formula'
 class BrewCaskGames < Formula
   homepage 'https://github.com/pr0d1r2/homebrew-cask-games/'
   url 'https://github.com/pr0d1r2/homebrew-cask-games.git'
+  version '1.0'
   def install
   end
 end
